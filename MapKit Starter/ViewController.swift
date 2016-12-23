da8f8da7528774b4c84903222d2f8b681a1db3e3
Dameon <dameongreen@gmail.com>
2016-12-22 23:00:15 -0600

    these are updates

--- MapKit Starter/ViewController.swift
+++ MapKit Starter/ViewController.swift
@@ -13,4 +13,85 @@ class ViewController: UIViewController {
     @IBOutlet var mapView: MKMapView?
     
     let locationManager = CLLocationManager()
+    let places = Place.getPlaces()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       // mapView?.showsUserLocation = true
+        requestLocationAccess()
+        
+        addAnnotations()
+        addPolyline()
+        addPolygon()
+    }
+    
+    func requestLocationAccess () {
+        let status = CLLocationManager.authorizationStatus()
+        
+        switch status {
+        case .denied, .restricted:
+            print("location access denied")
+        default :
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func addAnnotations() {
+        mapView?.delegate = self
+        mapView?.addAnnotations(places)
+        
+        let overlays = places.map { MKCircle(center: $0.coordinate, radius: 100) }
+        mapView?.addOverlays(overlays)
+        
+        var locations = places.map { $0.coordinate }
+        let polyline = MKPolyline(coordinates: &locations, count: locations.count)
+        mapView?.add(polyline)
+    }
+    
+    func addPolyline() {
+        var locations = places.map { $0.coordinate }
+        let polyline = MKPolyline(coordinates: &locations, count: locations.count)
+        
+        mapView?.add(polyline)
+    }
+    
+    func addPolygon() {
+        var locations = places.map { $0.coordinate }
+        let polygon = MKPolygon(coordinates: &locations, count: locations.count)
+        mapView?.add(polygon)
+        
+    }
+}
+
+extension ViewController: MKMapViewDelegate {
+  func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+            
+        else {
+            let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "annotationView") ?? MKAnnotationView()
+            annotationView.image = UIImage(named: "place icon")
+            return annotationView
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay is MKCircle {
+            let renderer = MKCircleRenderer(overlay: overlay)
+            renderer.fillColor = UIColor.black.withAlphaComponent(0.5)
+            renderer.strokeColor = UIColor.blue
+            renderer.lineWidth = 2
+            return renderer
+            
+        } else if overlay is MKPolyline {
+            let renderer = MKPolylineRenderer(overlay: overlay)
+            renderer.strokeColor = UIColor.orange
+            renderer.lineWidth = 3
+            return renderer
+        }
+        
+        return MKOverlayRenderer()
+    }
 }
